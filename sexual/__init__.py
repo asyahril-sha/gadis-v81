c2387797d63ddaeecb4ab1eef96b23f7cd4fc0ab
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 01:37:21 +0800

    Create __init__.py

---
+++ sexual/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+SEXUAL PACKAGE
+=============================================================================
+Konten dewasa untuk dinamika hubungan intim
+⚠️ Hanya untuk pengguna 18+
+"""
+
+from sexual.positions import SexPosition, POSITIONS
+from sexual.areas import SensitiveArea, AREAS
+from sexual.activities import SexualActivity, ACTIVITIES
+from sexual.climax import ClimaxVariation, CLIMAX_VARIATIONS
+from sexual.preferences import SexualPreferences
+from sexual.history import SexualHistory
+from sexual.ratings import PositionRating, AreaRating
+from sexual.compatibility import SexualCompatibility
+from sexual.fetishes import Fetish, FETISHES
+from sexual.toys import SexToy, TOYS
+from sexual.bdsm import BDSMLevel, BDSM_DYNAMICS
+
+__version__ = "81.0.0"
+__all__ = [
+    'SexPosition',
+    'POSITIONS',
+    'SensitiveArea',
+    'AREAS',
+    'SexualActivity',
+    'ACTIVITIES',
+    'ClimaxVariation',
+    'CLIMAX_VARIATIONS',
+    'SexualPreferences',
+    'SexualHistory',
+    'PositionRating',
+    'AreaRating',
+    'SexualCompatibility',
+    'Fetish',
+    'FETISHES',
+    'SexToy',
+    'TOYS',
+    'BDSMLevel',
+    'BDSM_DYNAMICS'
+]
