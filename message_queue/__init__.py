923b3db938b5c0beb31e3afe466b81214526821e
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 04:17:18 +0800

    Update __init__.py

--- message_queue/__init__.py
+++ message_queue/__init__.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+QUEUE PACKAGE
+=============================================================================
+Package untuk queue (RabbitMQ) - dikosongkan karena tidak dipakai
+"""
 
+# Tidak ada implementasi, hanya file kosong agar import tidak error
