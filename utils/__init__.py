22217080456fea5b6624b770c39af4f71453938b
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 05:07:37 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -15,13 +15,17 @@ from utils.exceptions import (
     SessionError,
     RoleError,
     ValidationError,
-    global_exception_handler,
+    global_exception_handler_obj,
     handle_errors
 )
+from utils.rate_limiter import RateLimiter, rate_limit, get_rate_limiter
 
 __all__ = [
+    # Logger
     'setup_logging',
     'logger',
+    
+    # Exceptions
     'GadisBaseException',
     'DatabaseError',
     'AIError',
@@ -29,6 +33,11 @@ __all__ = [
     'SessionError',
     'RoleError',
     'ValidationError',
-    'global_exception_handler',
-    'handle_errors'
+    'global_exception_handler_obj',
+    'handle_errors',
+    
+    # Rate Limiter
+    'RateLimiter',
+    'rate_limit',
+    'get_rate_limiter'
 ]
