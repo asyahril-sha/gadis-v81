cc8ef635108f088a2cd403cac5da5c5c95bd927d
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 02:49:20 +0800

    Create __init__.py

---
+++ cluster/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+CLUSTER PACKAGE
+=============================================================================
+Manajemen cluster untuk multiple instances (di-sederhanakan untuk single-user)
+"""
+
+# Untuk single-user, komponen cluster bisa di-sederhanakan
+# Semua fungsi akan return nilai default atau tidak melakukan apa-apa
+
+__version__ = "81.0.0"
+__all__ = []  # Tidak ada export untuk single-user
